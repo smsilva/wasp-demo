267b28f6b13a49172469fae7b979485d18f00c05
Silvio Silva <smsilva@gmail.com>
2022-05-04 19:37:40 -0300

    feat: Add Project item-relay

---
+++ item-relay/src/main/java/me/silvios/wasp/itemrelay/ItemRelayApplication.java
@@ -0,0 +1,13 @@
+package me.silvios.wasp.itemrelay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItemRelayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItemRelayApplication.class, args);
+	}
+
+}
