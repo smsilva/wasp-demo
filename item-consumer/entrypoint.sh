c2029526034293886718d26475a400c318d0823f
Silvio Silva <smsilva@gmail.com>
2023-04-17 10:53:17 -0300

    feat(item-consumer/): Update New Relic Environment

--- item-consumer/entrypoint.sh
+++ item-consumer/entrypoint.sh
@@ -2,6 +2,10 @@
 export JAVA_OPTS="${JAVA_OPTS} -javaagent:newrelic-agent.jar"
 export JAVA_OPTS="${JAVA_OPTS} -Dnewrelic.config.file=newrelic.yml"
 
+if [ -n "${NEW_RELIC_ENVIRONMENT}" ]; then
+  export JAVA_OPTS="${JAVA_OPTS} -Dnewrelic.environment=${NEW_RELIC_ENVIRONMENT}"
+fi
+
 echo "PWD........................: ${PWD}"
 echo "RABBITMQ_HOST..............: ${RABBITMQ_HOST}"
 echo "RABBITMQ_PORT..............: ${RABBITMQ_PORT}"
@@ -15,6 +19,7 @@ echo "NEW_RELIC_API_KEY..........: ${NEW_RELIC_API_KEY:0:10}"
 echo "NEW_RELIC_LICENSE_KEY......: ${NEW_RELIC_LICENSE_KEY:0:5}"
 echo "NEW_RELIC_LOG_LEVEL........: ${NEW_RELIC_LOG_LEVEL-fine}"
 echo "NEW_RELIC_APPENDER_ENABLED.: ${NEW_RELIC_LOG_LEVEL-fine}"
+echo "NEW_RELIC_ENVIRONMENT......: ${NEW_RELIC_ENVIRONMENT}"
 echo "JAVA_OPTS..................: ${JAVA_OPTS}"
 echo "SPRING_PROFILES_ACTIVE.....: ${SPRING_PROFILES_ACTIVE}"
 
